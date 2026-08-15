a1f54db357450faa9cec60eee997a6521f6dc136
anil-gadade18 <anilgadade2021@gmail.com>
2026-08-15 21:02:11 +0530

    added one endpoint in ConsumerController

--- consumer-service/src/main/java/com/consumer/consumer_service/controller/ConsumerController.java
+++ consumer-service/src/main/java/com/consumer/consumer_service/controller/ConsumerController.java
@@ -11,4 +11,10 @@ public class ConsumerController {
     public String getInstanceInfo(){
         return "Hello from consumer-service.";
     }
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello Everyone!!!";
+    }
+
 }
